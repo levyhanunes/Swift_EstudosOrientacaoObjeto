33e1a2a6985adb09b72a42c954dc0f034859577a
levyhanunes <levyhanunes@gmail.com>
2021-04-30 12:12:19 -0400

    Arrumar os MARK:

--- Swift_OrientacaoObjeto.playground/Contents.swift
+++ Swift_OrientacaoObjeto.playground/Contents.swift
@@ -3,42 +3,51 @@ import UIKit
 //MARK: - Classes e Metodos
 
 
+
+//MARK: NOVA CLASSE USUARIO
 class User {
+    
+    //MARK: - ATRIBUTOS
     var name : String
     var lastName : String
     var age: Int
     
+    //MARK: - INIT()
     init(name: String, lastName: String, age: Int) {
         self.name = name
         self.lastName = lastName
         self.age = age
     }
-    
+    //MARK: - METODOS
     func showFullName() -> Any{
         return "\(name) \(lastName) tem \(age) anos!"
     }
     
 }
 
-func returnUser() -> Any{
-    return User.self
-}
 
-print(returnUser())
-
-
+//MARK: CRIAR ONJETOS PASSANDO ARGUMENTOS NOME, SOBRENOME, ECT..
 var levy = User(name: "Levy", lastName: "Nunes", age: 23)
 var ruy = User(name: "Ruy", lastName: "Rodrigues", age: 18)
 
+//MARK: PRINT COM O METODO showFullName()
 print(levy.showFullName())
 
 
+
+
+
+//MARK: NOVA CLASSE CARRO
 class Car {
+    
+    //MARK: - Atributos
     var marca, modelo, cor : String
     var age : Int
     var ligado : Bool
     
+    //MARK: - INIT()
     init(marca: String, modelo: String, cor: String, age: Int, ligado: Bool) {
+        
         self.marca = marca
         self.modelo = modelo
         self.cor = cor
@@ -46,6 +55,7 @@ class Car {
         self.ligado = ligado
     }
     
+    //MARK: - Metodos
     func showCar() -> Any {
         return "Marca: \(marca) Modelo: \(modelo) Cor: \(cor) Ano: \(age)"
     }
@@ -59,6 +69,9 @@ class Car {
     }
 }
 
+//MARK: - CRIAR OBJETO
 var porsche = Car(marca: "Porsche", modelo: "Caynne", cor: "Amarelo", age: 2020, ligado: true)
 
+
+//MARK: - PRINT FUNCAO carroLigado()
 print(porsche.estaLigado())
